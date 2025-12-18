e8d411da44fe9a92550ea3fe70cee0977e0c77cb
bala-amypo <bala@amypo.in>
2025-12-18 04:00:34 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Influencer.java
@@ -0,0 +1,24 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Influencer;
+
+@Service
+public class InfluencerService {
+
+    private Map<Integer, Influencer> details = new HashMap<>();
+    public Influencer addInfluencer(Influencer influencer) {
+        details.put(influencer.getId(), influencer);
+        return details.get(influencer.getId());
+    }
+
+    public List<Influencer> getAllInfluencers() {
+        return new ArrayList<>(details.values());
+    }
+}
