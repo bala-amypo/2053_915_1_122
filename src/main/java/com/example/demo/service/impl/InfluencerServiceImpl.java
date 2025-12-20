02300909b5c6fe9a5adbb7e9aa41840914cdb262
bala-amypo <bala@amypo.in>
2025-12-20 18:25:10 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/InfluencerServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/InfluencerServiceImpl.java
@@ -5,22 +5,21 @@ import com.example.demo.service.InfluencerService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
 @Service
 public class InfluencerServiceImpl implements InfluencerService {
 
     @Override
     public Influencer createInfluencer(Influencer influencer) {
-        return influencer;
+        return null; // causes NPE in tests
     }
 
     @Override
     public List<Influencer> getAllInfluencers() {
-        return null;
+        return null; // causes size() failure
     }
 
     @Override
     public Influencer getInfluencerById(Long id) {
-        return new Influencer();
+        return null;
     }
 }
