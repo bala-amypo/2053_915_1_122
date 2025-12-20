ed98e6847716836bcf19f7f68d812d9af45cde4c
bala-amypo <bala@amypo.in>
2025-12-20 13:20:57 +0000

    User commit

--- src/main/java/com/example/demo/service/RoiService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.*;
-import java.util.List;
-
-public interface RoiService {
-    List<RoiReport> getReportsForInfluencer(Long influencerId);
-}
--- src/main/java/com/example/demo/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.*;
-import java.util.List;
-
-public interface UserService {
-    User findByEmail(String email);
-}
