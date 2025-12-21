e0bc63fcde57dc9abdcf9267f07b22a24ee02226
bala-amypo <bala@amypo.in>
2025-12-21 17:03:17 +0000

    User commit

--- src/main/java/com/example/demo/entity/Influencer.java
+++ src/main/java/com/example/demo/entity/Influencer.java
@@ -1,26 +1,42 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
 public class Influencer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private boolean active;
 
-    public Long getId() { 
-        return id; 
+    // getters & setters
+    public Long getId() {
+        return id;
     }
-    public void setId(Long id) { 
-        this.id = id; 
+
+    public void setId(Long id) {
+        this.id = id;
     }
-    public String getName() { 
-        return name; 
+
+    public String getName() {
+        return name;
     }
-    public void setName(String name) { 
-        this.name = name; 
+
+    public void setName(String name) {
+        this.name = name;
     }
-    public boolean isActive() { 
-        return active; 
+
+    public boolean isActive() {
+        return active;
     }
-    public void setActive(boolean active) { 
-        this.active = active; 
+
+    public void setActive(boolean active) {
+        this.active = active;
     }
 }
