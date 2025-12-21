8ec50dc6051731c562405205a65191072bcf80b9
bala-amypo <bala@amypo.in>
2025-12-21 15:06:31 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -3,24 +3,3 @@ package com.example.demo.security;
 public class JwtUtil {
 
 }
-package com.example.demo.config;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
-
-@Configuration
-public class SwaggerConfig {
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .servers(List.of(
-                        // change port if your app runs on a different port
-                        new Server().url("http://localhost:9001")
-                ));
-    }
-}
