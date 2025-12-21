68a0832d5d71a71d99cb321faa387c252367d56a
bala-amypo <bala@amypo.in>
2025-12-21 14:49:20 +0000

    User commit

--- src/main/java/com/example/demo/controller/CampaignController.java
+++ src/main/java/com/example/demo/controller/CampaignController.java
@@ -2,9 +2,6 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Campaign;
 import com.example.demo.service.CampaignService;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,15 +15,15 @@ public class CampaignController {
         this.service = service;
     }
 
-    public ResponseEntity<Campaign> updateCampaign(Long id, Campaign c) {
-        return new ResponseEntity<>(null, HttpStatus.OK);
+    public Campaign updateCampaign(Long id, Campaign c) {
+        return service.updateCampaign(id, c);
     }
 
-    public ResponseEntity<Campaign> getCampaign(Long id) {
-        return new ResponseEntity<>(null, HttpStatus.OK);
+    public Campaign getCampaign(Long id) {
+        return service.getCampaignById(id);
     }
 
-    public ResponseEntity<List<Campaign>> getAllCampaigns() {
-        return new ResponseEntity<>(null, HttpStatus.OK);
+    public List<Campaign> getAllCampaigns() {
+        return service.getAllCampaigns();
     }
 }
