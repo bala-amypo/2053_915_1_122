bfb380c2bb71d3823cbe3b8752e2842414609996
bala-amypo <bala@amypo.in>
2025-12-21 17:12:17 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,34 +1,56 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
 import java.time.LocalDateTime;
 
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String email;
+
     private String role;
+
     private LocalDateTime createdAt;
 
+    // getters and setters
     public Long getId() { 
         return id;
-     }
+    }
+
     public void setId(Long id) {
         this.id = id; 
     }
+
     public String getEmail() { 
         return email; 
     }
+
     public void setEmail(String email) { 
         this.email = email; 
     }
+
     public String getRole() { 
         return role; 
     }
+
     public void setRole(String role) { 
         this.role = role; 
     }
+
     public LocalDateTime getCreatedAt() { 
         return createdAt; 
     }
+
     public void setCreatedAt(LocalDateTime createdAt) { 
         this.createdAt = createdAt; 
     }
