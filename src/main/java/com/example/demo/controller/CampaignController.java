d3abcb181268b62fda2a5d46439fdb074b032705
bala-amypo <bala@amypo.in>
2025-12-21 14:57:31 +0000

    User commit

--- src/main/java/com/example/demo/controller/CampaignController.java
+++ src/main/java/com/example/demo/controller/CampaignController.java
@@ -2,11 +2,12 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Campaign;
 import com.example.demo.service.CampaignService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/campaigns")   // Base URL
 public class CampaignController {
 
     CampaignService service;
@@ -15,14 +16,21 @@ public class CampaignController {
         this.service = service;
     }
 
-    public Campaign updateCampaign(Long id, Campaign c) {
+    // UPDATE campaign
+    @PutMapping("/{id}")
+    public Campaign updateCampaign(@PathVariable Long id,
+                                   @RequestBody Campaign c) {
         return service.updateCampaign(id, c);
     }
 
-    public Campaign getCampaign(Long id) {
+    // GET campaign by ID
+    @GetMapping("/{id}")
+    public Campaign getCampaign(@PathVariable Long id) {
         return service.getCampaignById(id);
     }
 
+    // GET all campaigns
+    @GetMapping
     public List<Campaign> getAllCampaigns() {
         return service.getAllCampaigns();
     }
