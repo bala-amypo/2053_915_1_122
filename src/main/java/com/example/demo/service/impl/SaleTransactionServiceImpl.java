fa6569286a447e5e5233649cc70f5856127455c4
bala-amypo <bala@amypo.in>
2025-12-22 10:26:58 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/SaleTransactionServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SaleTransactionServiceImpl.java
@@ -1,15 +1,10 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.model.Influencer;
-import com.example.demo.model.SaleTransaction;
-import com.example.demo.repository.InfluencerRepository;
-import com.example.demo.repository.SaleTransactionRepository;
+import com.example.demo.model.*;
+import com.example.demo.repository.*;
 import com.example.demo.service.SaleTransactionService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class SaleTransactionServiceImpl implements SaleTransactionService {
@@ -20,33 +15,20 @@ public class SaleTransactionServiceImpl implements SaleTransactionService {
     @Autowired
     private InfluencerRepository influencerRepo;
 
+    @Autowired
+    private CampaignRepository campaignRepo;
+
     @Override
     public SaleTransaction createSale(SaleTransaction tx) {
 
         Long influencerId =
                 tx.getDiscountCode().getInfluencer().getId();
 
-        Influencer influencer =
-                influencerRepo.findById(influencerId)
-                        .orElseThrow(() -> new RuntimeException("Influencer not found"));
+        Influencer influencer = influencerRepo.findById(influencerId)
+                .orElseThrow(() -> new RuntimeException("Influencer not found"));
 
         tx.getDiscountCode().setInfluencer(influencer);
 
         return saleRepo.save(tx);
-    }
-
-    @Override
-    public List<SaleTransaction> getSalesForCode(Long codeId) {
-        return List.of();
-    }
-
-    @Override
-    public List<SaleTransaction> getSalesForInfluencer(Long influencerId) {
-        return List.of();
-    }
-
-    @Override
-    public List<SaleTransaction> getSalesForCampaign(Long campaignId) {
-        return List.of();
     }
 }
