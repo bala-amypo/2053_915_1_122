e2b831618e28574bd600c43657e7ed2bded9d4b2
bala-amypo <bala@amypo.in>
2025-12-22 08:14:24 +0000

    User commit

--- src/main/java/com/example/demo/service/InfluencerService.java
+++ src/main/java/com/example/demo/service/InfluencerService.java
@@ -1,10 +1,13 @@
 package com.example.demo.service;
 
-import com.example.demo.model.*;
+import com.example.demo.model.Influencer;
 import java.util.List;
 
 public interface InfluencerService {
+
     Influencer createInfluencer(Influencer influencer);
-    List<Influencer> getAllInfluencers();
+
     Influencer getInfluencerById(Long id);
+
+    List<Influencer> getAllInfluencers();
 }
--- src/main/java/com/example/demo/service/impl/InfluencerServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/InfluencerServiceImpl.java
@@ -1,24 +1,31 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.model.Influencer;
+import com.example.demo.repository.InfluencerRepository;
 import com.example.demo.service.InfluencerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+
 @Service
 public class InfluencerServiceImpl implements InfluencerService {
 
     @Autowired
-    private InfluencerRepository repository;
+    private InfluencerRepository influencerRepository;
 
     @Override
     public Influencer createInfluencer(Influencer influencer) {
-        return repository.save(influencer); // ✅ SAVED
+        return influencerRepository.save(influencer);
     }
 
     @Override
     public Influencer getInfluencerById(Long id) {
-        return repository.findById(id).orElse(null); // ✅ FETCHED
+        return influencerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Influencer> getAllInfluencers() {
+        return influencerRepository.findAll();
     }
 }
