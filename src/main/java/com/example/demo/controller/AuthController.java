6d9b97f999fe3ac69212528961dd6bd0ff863418
bala-amypo <bala@amypo.in>
2025-12-22 10:43:23 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,16 +1,12 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.security.JwtUtil;
 
-import com.example.demo.service.UserService;
-
-@RestController
 public class AuthController {
 
-    
-    private final UserService userService;
-    public AuthController(UserService userService){
-        this.userService=userService;
+    private final JwtUtil jwtUtil;
+
+    public AuthController(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
     }
 }
--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -2,4 +2,23 @@ package com.example.demo.security;
 
 public class JwtUtil {
 
+    public String generateToken(String email, String role, Long userId) {
+        return "token";
+    }
+
+    public boolean validateToken(String token) {
+        return true;
+    }
+
+    public String extractEmail(String token) {
+        return "email";
+    }
+
+    public String extractRole(String token) {
+        return "role";
+    }
+
+    public Long extractUserId(String token) {
+        return 1L;
+    }
 }
