826c8c07ec8584fb789c5ca39c89d1d83f47436a
bala-amypo <bala@amypo.in>
2025-12-23 03:33:51 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(
+    basePackages = "com.example.demo",
+    excludeFilters = {
+        @ComponentScan.Filter(
+            type = FilterType.REGEX,
+            pattern = "com.example.demo.(controller|service|repository).*"
+        )
+    }
+)
 public class DemoApplication {
 
     public static void main(String[] args) {
